52f1b7acb8c26aff59079ba86e5297630504320c
Nghi Le <vinhnghi223@gmail.com>
2022-01-18 10:19:02 +0200

    use class instead of id in about me

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -354,16 +354,16 @@ app.controller('menuLink', ['$scope','$location',function($scope,$location) {
 
 app.controller('about-me', ['$scope', function($scope) {
   var s = skrollr.init();
-  $("#avt").click(function() {
+  $(".avt").click(function() {
       $('html, body').animate({
           scrollTop: 310
       }, 400);
   });
 
-  $("#avt").hover(function() {
-      $('#intro h1, #intro p').addClass("moving");
+  $(".avt").hover(function() {
+      $('.intro h1, .intro p').addClass("moving");
   },function() {
-      $('#intro h1, #intro p').removeClass("moving");
+      $('.intro h1, .intro p').removeClass("moving");
   });
 
   
@@ -373,29 +373,29 @@ app.controller('about-me', ['$scope', function($scope) {
     //console.log($(this).scrollTop());
     //Slide 1
     if ($(this).scrollTop()>260){
-        $('#slide-1 #intro').fadeOut();
+        $('.slide-1 .intro').fadeOut();
      }else{
-      $('#slide-1 #intro').fadeIn();
+      $('.slide-1 .intro').fadeIn();
     }
 
     //Slide 2
     if ($(this).scrollTop()>=300 && $(this).scrollTop()<800){
-        $('#slide-2 .caption.part1').fadeIn();
+        $('.slide-2 .caption.part1').fadeIn();
     }else{     
-        $('#slide-2 .caption.part1').fadeOut();
+        $('.slide-2 .caption.part1').fadeOut();
     }
 
     if ($(this).scrollTop()>=800 && $(this).scrollTop()<1300){
-        $('#slide-2 .caption.part2').fadeIn();
+        $('.slide-2 .caption.part2').fadeIn();
     }else{     
-        $('#slide-2 .caption.part2').fadeOut();
+        $('.slide-2 .caption.part2').fadeOut();
     }      
 
     //Slide 3
     if ($(this).scrollTop()>1450 && $(this).scrollTop()<2300){
-        $('#slide-3 .caption').fadeIn();
+        $('.slide-3 .caption').fadeIn();
     }else{     
-      $('#slide-3 .caption').fadeOut();
+      $('.slide-3 .caption').fadeOut();
     }
    
    });
